cb767bfbf77f32a42dc1e7af1e1441076ae7a175
ram <manikandan.it23@gmail.com>
2023-04-28 19:38:20 +0930

    standalonetrial

---
+++ startup.bat
@@ -0,0 +1,1 @@
+set HOST_PORT=3000&&set WRITE_DB_SRV_HOST=localhost&&set WRITE_DB_USER=root&&set WRITE_DB_PASSWORD=Ashrith@26&&set WRITE_DB_DATABASE=swastha_hms&&set WRITE_DB_PORT=3306&&pm2 start index.js
